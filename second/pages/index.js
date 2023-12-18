31ccd576bc6896e828cba98e50fe126de9d7de9f
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 02:31:14 +0530

    Create index.js

---
+++ second/pages/index.js
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import MeetupList from '../components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
+
+function HomePage(props) {
+  return (
+    <>
+      <Head>
+        <title>Meetups Home Page</title>
+        <meta
+          name='description'
+          content='One of the best online meetup website'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        id: meetup._id.toString(),
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
